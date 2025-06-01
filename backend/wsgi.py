eecc4afb6d87d136e7ec915a5bbc230a976d2ea5
gptfleet <gptfleet@gmail.com>
2025-06-01 02:03:25 +0000

    Update

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+from .mongo_connection import connect_mongodb
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = get_wsgi_application()
+
+connect_mongodb()
