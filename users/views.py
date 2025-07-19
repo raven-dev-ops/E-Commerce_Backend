1aebdfc33bb5233691a6c67de38e8b3bf5f71bcf
gptfleet <gptfleet@gmail.com>
2025-07-19 20:56:05 +0000

    Update

--- users/views.py
+++ users/views.py
@@ -1,13 +1,14 @@
+# users/views.py
+
 from rest_framework import generics, permissions
-from django.contrib.auth import get_user_model
 from rest_framework.serializers import ModelSerializer, CharField
-from dj_rest_auth.registration.views import SocialLoginView
+from django.contrib.auth import get_user_model
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
+from dj_rest_auth.registration.views import SocialLoginView
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 
 User = get_user_model()
 
-# User Serializer
 class UserSerializer(ModelSerializer):
     password = CharField(write_only=True)
 
@@ -31,13 +32,11 @@ class UserSerializer(ModelSerializer):
         instance.save()
         return instance
 
-# Register endpoint
 class RegisterUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [permissions.AllowAny]
 
-# Profile endpoint
 class UserProfileView(generics.RetrieveUpdateAPIView):
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
@@ -45,17 +44,8 @@ class UserProfileView(generics.RetrieveUpdateAPIView):
     def get_object(self):
         return self.request.user
 
-# Custom Google OAuth2 login
 class CustomGoogleLogin(SocialLoginView):
     adapter_class = GoogleOAuth2Adapter
-    client_class = OAuth2Client
-    callback_url = "https://twiinz-beard-frontend.netlify.app"
 
-    def get_client_class_kwargs(self):
-        """
-        Fixes TypeError for multiple values of 'scope_delimiter'
-        """
-        return {
-            "request": self.request,
-            "callback_url": self.callback_url,
-        }
+    def get_callback_url(self):
+        return "https://twiinz-beard-frontend.netlify.app"
