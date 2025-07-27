e5b8f9944782753c8bcdd8c065b1f75518007098
gptfleet <gptfleet@gmail.com>
2025-07-27 01:07:15 +0000

    Update

--- orders/urls.py
+++ orders/urls.py
@@ -1,9 +1,12 @@
 # orders/urls.py
 
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import OrderViewSet
 
 router = DefaultRouter()
-router.register(r'orders', OrderViewSet, basename='order')
+router.register(r'', OrderViewSet, basename='order')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('', include(router.urls)),
+]
